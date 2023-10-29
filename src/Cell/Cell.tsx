610b9ae6911d2952175345ebc54229eecbb63a55
Dominik.Kotecki <dominikus1910@gmail.com>
2023-10-29 08:08:31 +0100

    .

--- src/Cell/Cell.tsx
+++ src/Cell/Cell.tsx
@@ -1,4 +1,4 @@
 
 export default function Cell(props: { onClick: () => void, value: string }) {
-    return <button onClick={props.onClick}>{props.value}</button>
+    return <button className="cell" onClick={props.onClick}>{props.value}</button>
 }
