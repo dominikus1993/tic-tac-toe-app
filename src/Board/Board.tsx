5bb5ea02577f863d4cd0117c4275e5687ff5268a
Dominik.Kotecki <dominikus1910@gmail.com>
2023-10-31 18:44:05 +0100

    add Cell

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -2,5 +2,21 @@ import Cell from "../Cell/Cell";
 
 
 export default function Board() {
-    return <Cell onClick={() => console.log("test")} value="X"></Cell>
+    return <table>
+    <tr>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+    </tr>
+    <tr>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+    </tr>
+    <tr>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+      <td><Cell onClick={() => {}} value="1"></Cell></td>
+    </tr>
+  </table>
 }
