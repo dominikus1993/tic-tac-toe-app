d4248e53afe218e1a331162f239fd843378c9b87
Dominik.Kotecki <dominikus1910@gmail.com>
2023-11-01 08:58:57 +0100

    tic tac toe

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,42 +1,106 @@
 import { useState } from "react";
 import Cell from "../Cell/Cell";
 
-const defaultBoard: { [x: number]: string } = {
-    0: "X",
+type Board = { [x: number]: string }
+type Player = "X" | "O"
+const defaultBoard: Board = {
+    0: "",
     1: "",
     2: "",
     3: "",
     4: "",
     5: "",
-    6: "X",
+    6: "",
     7: "",
     8: "",
 }
 
-export default function Board() {
-    let [squares, setSquares] = useState({ ...defaultBoard})
+type BoardState = {
+    board: Board,
+    currentPlayer: Player,
+    winner: Player | null
+}
 
-    function handleClick(id: number) {
-        const newSquares = { ...squares }
-        newSquares[id] = "X"
-        setSquares(newSquares)
+const possibleWinningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+]
+
+
+function getBoardCell(state: BoardState, id: number): string {
+    return state.board[id];
+}
+
+function calcWinner(state: BoardState): Player | null {
+    for (const combination of possibleWinningCombinations) {
+        const [a, b, c] = combination;
+        if (state.board[a] === state.board[b] && state.board[a] === state.board[c] && state.board[a] !== "") {
+            return state.board[a] as Player;
+        }
+    }
+    return null;
+}
+
+function canMove(state: BoardState, id: number): boolean {
+    if (state.winner !== null) {
+        return false;
     }
 
-    return <table>
-    <tr>
-      <td><Cell id={0} onClick={(id) => handleClick(id)} value={squares[0]}></Cell></td>
-      <td><Cell id={1} onClick={(id) => handleClick(id)} value={squares[1]}></Cell></td>
-      <td><Cell id={2} onClick={(id) => handleClick(id)} value={squares[2]}></Cell></td>
-    </tr>
-    <tr>
-      <td><Cell id={3} onClick={(id) => handleClick(id)} value={squares[3]}></Cell></td>
-      <td><Cell id={4} onClick={(id) => handleClick(id)} value={squares[4]}></Cell></td>
-      <td><Cell id={5} onClick={(id) => handleClick(id)} value={squares[5]}></Cell></td>
-    </tr>
-    <tr>
-      <td><Cell id={6} onClick={(id) => handleClick(id)} value={squares[6]}></Cell></td>
-      <td><Cell id={7} onClick={(id) => handleClick(id)} value={squares[7]}></Cell></td>
-      <td><Cell id={8} onClick={(id) => handleClick(id)} value={squares[8]}></Cell></td>
-    </tr>
-  </table>
+    if (getBoardCell(state, id) !== "") {
+        return false;
+    }
+    return true;
+}
+
+
+export default function Board() {
+    let [status, setStatus] = useState("Next player: X")
+    let [board, setBoard] = useState({ board: { ...defaultBoard }, currentPlayer: "X", winner: null } as BoardState)
+
+    function handleClick(id: number) {
+        if (!canMove(board, id)) {
+            return;
+        }
+
+        const newBoard = { ...board }
+        newBoard.board[id] = board.currentPlayer;
+        newBoard.currentPlayer = board.currentPlayer === "X" ? "O" : "X"
+
+        newBoard.winner = calcWinner(newBoard);
+
+        setBoard(newBoard)
+
+        if (newBoard.winner !== null) {
+            setStatus("Winner: " + newBoard.winner)
+        } else {
+            setStatus("Next player: " + newBoard.currentPlayer)
+        }
+    }
+
+    return<>
+        <div>{status}</div>
+        <table>
+            <tr>
+                <td><Cell id={0} onClick={(id) => handleClick(id)} value={getBoardCell(board, 0)}></Cell></td>
+                <td><Cell id={1} onClick={(id) => handleClick(id)} value={getBoardCell(board, 1)}></Cell></td>
+                <td><Cell id={2} onClick={(id) => handleClick(id)} value={getBoardCell(board, 2)}></Cell></td>
+            </tr>
+            <tr>
+                <td><Cell id={3} onClick={(id) => handleClick(id)} value={getBoardCell(board, 3)}></Cell></td>
+                <td><Cell id={4} onClick={(id) => handleClick(id)} value={getBoardCell(board, 4)}></Cell></td>
+                <td><Cell id={5} onClick={(id) => handleClick(id)} value={getBoardCell(board, 5)}></Cell></td>
+            </tr>
+            <tr>
+                <td><Cell id={6} onClick={(id) => handleClick(id)} value={getBoardCell(board, 6)}></Cell></td>
+                <td><Cell id={7} onClick={(id) => handleClick(id)} value={getBoardCell(board, 7)}></Cell></td>
+                <td><Cell id={8} onClick={(id) => handleClick(id)} value={getBoardCell(board, 8)}></Cell></td>
+            </tr>
+        </table>
+    </>
 }
